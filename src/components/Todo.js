b81b3aadedcdebef554cf47445f6cbbd0bfffc0b
Alena <golovkoalena092@gmail.com>
2020-07-29 00:10:17 +0300

    reduce

---
+++ src/components/Todo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Todo = ({onClick, completed, text}) => {
+  console.log(onClick)
+  return(
+    <li
+      onClick={onClick}
+      style={{
+        textDecoration: completed ? 'line-through' : 'none'
+      }}
+    >
+      {text}
+    </li>
+  );
+}
+
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired
+}
+
+export default Todo
